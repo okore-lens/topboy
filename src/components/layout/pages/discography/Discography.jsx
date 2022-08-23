1e32b0dff2cfec6c96cde80320dd78fe31239de2
Infiinite-lens <okorelens27@gmail.com>
2022-08-23 16:01:32 +0300

    Messages

--- src/components/layout/pages/discography/Discography.jsx
+++ src/components/layout/pages/discography/Discography.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AudioCard from "../../../cards/audioCard/AudioCard";
 import VideoCard from "../../../cards/videoCard/VideoCard";
 
 import "./Discography.scss";
 
 function Discography() {
+  const [videos, setVideos] = useState([]);
+  // fetch videos
+  const fetchVideos = async () => {
+    const response = await fetch(
+      "https://topboy-nation-default-rtdb.europe-west1.firebasedatabase.app/Videos.json"
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    const responseData = await response.json();
+
+    const loadedVideos = [];
+
+    for (const key in responseData) {
+      loadedVideos.push({
+        id: key,
+        src: responseData[key].src,
+      });
+    }
+
+    loadedVideos.map((vid) => {
+      let str = vid.src;
+      let after = str.replace(/"/g, "");
+      vid.src = after;
+      console.log(vid.src);
+    });
+
+    setVideos(loadedVideos);
+  };
+
+  useEffect(() => {
+    fetchVideos().catch((error) => alert(error.message));
+  }, []);
+
   return (
     <div className="Discography">
       <h2>DISCOGRAPHY</h2>
@@ -33,6 +68,7 @@ function Discography() {
         adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna ali
       </p>
+
       <div className="audio">
         <h3>AUDIO MIXES</h3>
         <AudioCard />
@@ -42,9 +78,9 @@ function Discography() {
       </div>
       <div className="video">
         <h3>VIDEO MIXES</h3>
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
+        {videos.map((video) => (
+          <VideoCard key={video.id} src={video.src} />
+        ))}
       </div>
     </div>
   );

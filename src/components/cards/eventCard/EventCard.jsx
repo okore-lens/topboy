1768c323eda3478102dcb88a155e697304d698eb
piusgori <piusgori@gmail.com>
2022-10-01 21:45:10 +0300

    Created logic for separating day and month

--- src/components/cards/eventCard/EventCard.jsx
+++ src/components/cards/eventCard/EventCard.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import image from "../../../assets/images/about-img.png";
 import iconLocation from "../../../assets/images/icon-location.png";
 import iconShare from "../../../assets/images/icon-share.png";
+import { getMonth } from "../../../middleware/months";
 
 import "./EventCard.scss";
 
 function EventCard(props) {
+
+  const datesArray = props.day.split('-');
+  const day = Number(datesArray[2]);
+  const month = getMonth(Number(datesArray[1]));
+
   return (
     <div className="EventCard">
       <div className="date">
@@ -13,8 +19,8 @@ function EventCard(props) {
           <div className="poster">
             <img src={`http://localhost:8000/images/${props.poster}`} />
           </div>
-          <div className="day">{props.day}</div>
-          <div className="month">{props.month}</div>
+          <div className="day">{day}</div>
+          <div className="month">{month}</div>
         </div>
       </div>
       <div className="details">
---
+++ src/middleware/months.js
@@ -0,0 +1,4 @@
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+export const getMonth = (monthDay) => {
+    return months[monthDay - 1];
+}
